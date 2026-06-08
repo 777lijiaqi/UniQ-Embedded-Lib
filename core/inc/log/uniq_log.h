0024a7ae42f522a8ed4392aa588167201cda4ad5
lijiaqi777 <143959999+777lijiaqi@users.noreply.github.com>
2026-06-08 11:16:12 +0800

    Create uniq_log.h

---
+++ core/inc/log/uniq_log.h
@@ -0,0 +1,29 @@
+#ifndef __UNIQ_LOG_H
+#define __UNIQ_LOG_H
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdarg.h>
+
+#define LOG_TX_BUFF_SIZE    256
+#define LOG_RX_BUFF_SIZE    256
+#define LOG_TIME_OUT        0xFFFF
+typedef enum{
+    LOG_OK,
+    LOG_TIMEOUT,
+    LOG_ERROR,
+    LOG_BUSY
+}logState_t;
+
+typedef logState_t (*log_transmit)(void* hardware , const uint8_t* pData , uint16_t size , uint32_t timOut);
+typedef logState_t (*log_receive)(void* hardware , const uint8_t* pData , uint16_t size , uint32_t timOut);
+
+typedef struct{
+    void* hardware;
+    void* log_hardware_init(void);
+    log_transmit transmitFunc;
+    log_receive receiveFunc;
+    void* log_hardware_deinit(void);
+}logFunc_t;
+
+#endif
